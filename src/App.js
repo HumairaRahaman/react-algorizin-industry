03b1f03c1ba720da24962c0696db431bbaf741cb
Humaira Rahaman <humairamitul@gmail.com>
2022-04-27 03:13:43 +0600

    stater teamplate

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Homepage from './components/Homepage/Homepage'
+
+const App = () => {
+  return (
+    <div>
+      <Header></Header>
+     <Homepage></Homepage>
+     
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <h2>Comming soon</h2>
+  )
+}
+
+export default Homepage
